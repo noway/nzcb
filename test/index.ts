19ea1e8a03e29c52c9fe0ba5169c4b92911d716b
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-10 22:02:44 +1300

    tidy up

--- test/index.ts
+++ test/index.ts
@@ -7,12 +7,8 @@ describe("Greeter", function () {
     const greeter = await Greeter.deploy("NZ COVID Badge", "NZCP");
     await greeter.deployed();
 
-    // expect(
-    const setGreetingTx = await greeter.mint([0,0],[[0,0],[0,0]],[0,0],[0,0,0],0,0,0)//).to.equal("Hello, world!");
+    const setGreetingTx = await greeter.mint([0,0],[[0,0],[0,0]],[0,0],[0,0,0],0,0,0)
 
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
     await setGreetingTx.wait();
 
     expect(await greeter.tokenURI(0)).to.equal("https://i.imgur.com/QYKQsql.jpg");
