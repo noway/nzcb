59d4615d5bca627b1af289d6dfa5c6cdba4a9b54
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-10 23:03:08 +1300

    tidy up

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
 
-import "hardhat/console.sol";
 import "./ERC721.sol";
 import "./verifier_example.sol";
 import "./EllipticCurve.sol";
@@ -10,8 +9,7 @@ contract Greeter is ERC721, Verifier, EllipticCurve {
 
     uint public supply;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
-    }
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function totalSupply() public view returns (uint) {
         return supply;
@@ -22,7 +20,6 @@ contract Greeter is ERC721, Verifier, EllipticCurve {
             uint[2][2] memory b,
             uint[2] memory c,
             uint[3] memory input, bytes32 toBeSignedHash, uint256[2] memory rs) public payable {
-
 
         uint exampleX = 0xCD147E5C6B02A75D95BDB82E8B80C3E8EE9CAA685F3EE5CC862D4EC4F97CEFAD;
         uint exampleY = 0x22FE5253A16E5BE4D1621E7F18EAC995C57F82917F1A9150842383F0B4A4DD3D;
