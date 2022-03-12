195d89af01ed7349ebaf0877622d25cf8e7984f3
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-12 21:47:52 +1300

    uint256 is cheaper

--- contracts/NZCovidBadge.sol
+++ contracts/NZCovidBadge.sol
@@ -9,7 +9,7 @@ import "./EllipticCurve.sol";
 contract NZCOVIDBadge is ERC721, Verifier, EllipticCurve {
 
     uint public supply;
-    mapping(bytes32 => bool) public minted;
+    mapping(bytes32 => uint256) public minted;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name = "NZ COVID Badge", _symbol = "NZCB") {}
 
@@ -114,9 +114,9 @@ contract NZCOVIDBadge is ERC721, Verifier, EllipticCurve {
         require(verifyProof(a, b, c, input), "Invalid proof");
         require(validateSignature(toBeSignedHash, rs, [0xCD147E5C6B02A75D95BDB82E8B80C3E8EE9CAA685F3EE5CC862D4EC4F97CEFAD, 0x22FE5253A16E5BE4D1621E7F18EAC995C57F82917F1A9150842383F0B4A4DD3D]), "Invalid signature");
         require(block.timestamp < _exp, "Pass expired");
-        require(minted[credSubjHash] == false, "Already minted");
+        require(minted[credSubjHash] == 0, "Already minted");
 
-        minted[credSubjHash] = true;
+        minted[credSubjHash] = 1;
         _safeMint(addr, supply);
         supply++;
     }
