b2f3bbdc9101d119adb636147faf890dc32ee559
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-14 22:18:52 +1300

    deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,6 +38,10 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    ganache: {
+      url: `http://127.0.0.1:7545`,
+      accounts: [`${process.env.GANACHE_PRIVATE_KEY}`]
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
