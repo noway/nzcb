30de415bbab0c9628071f41fb06bf940a0ec41cc
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-04-11 02:56:26 +1200

    proper deployment procedure

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,11 +4,18 @@ async function main() {
   const PlonkVerifier = await ethers.getContractFactory("PlonkVerifier");
   const plonk = await PlonkVerifier.deploy();
 
+  await plonk.deployed();
+
   const NZCOVIDBadge = await ethers.getContractFactory("NZCOVIDBadge");
-  const covidBadge = await NZCOVIDBadge.deploy("NZ COVID Badge", "NZCP", plonk.address);
+  const covidBadge = await NZCOVIDBadge.deploy(
+    "NZ COVID Badge",
+    "NZCP",
+    plonk.address
+  );
 
   await covidBadge.deployed();
 
+  console.log("PlonkVerifier deployed to:", plonk.address);
   console.log("NZCOVIDBadge deployed to:", covidBadge.address);
 }
 
